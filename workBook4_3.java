65860ebed36828cb955f54c2451eb6cb608ec6ec
eliya <eliyaalonn1@gmail.com>
2024-02-10 22:24:26 +0200

    pokerito

---
+++ workBook4_3.java
@@ -0,0 +1,20 @@
+public class workBook4_3 {
+
+  public static void main(String[] args) {
+    printTemperatures(77);
+    printTemperatures(61);
+    printTemperatures(55);
+
+  }
+
+  public static double fahrenheitToCelsius(double fahrenheit) {
+    double celsius = (fahrenheit - 32) * 5 / 9;
+
+    return celsius;
+  }
+
+  public static void printTemperatures(double fahrenheit) {
+    System.out.println("F: " + fahrenheit);
+    System.out.println(fahrenheitToCelsius(fahrenheit));
+  }
+}
